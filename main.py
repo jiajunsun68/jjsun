1031a43d4f226bb2e4c7ad16cf7a3b89cd019708
jiajunsun68 <72682682+jiajunsun68@users.noreply.github.com>
2022-08-24 19:01:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -39,6 +39,7 @@ def get_words():
   words = requests.get("https://api.shadiao.pro/chp")
   if words.status_code != 200:
     return get_words()
+  print(words.json()['data']['text'])
   return words.json()['data']['text']
 
 def get_random_color():
